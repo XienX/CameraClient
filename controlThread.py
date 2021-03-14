e61ce2cbf88f018a943df29474e395005871279f
Xien <1324548879@qq.com>
2021-03-14 17:56:36 +0800

    登录验证

---
+++ controlThread.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+# @Time : 2021/3/11 16:44
+# @Author : XieXin
+# @Email : 1324548879@qq.com
+# @File : controlThread.py
+# @notice ：
+
+import json
+import socket
+
+import numpy as np
+from PyQt5 import QtCore
+from PyQt5.QtCore import *
+
+
+# 继承QThread
+class ControlThread(QtCore.QThread):
+    #  通过类成员对象定义信号对象
+    frame_signal = pyqtSignal(np.ndarray)
+    log_signal = pyqtSignal(str)
+    connect_button_signal = pyqtSignal(bool)
+    close_button_signal = pyqtSignal(bool)
+
+    def __init__(self, user_name, password, ip, port):
+        super().__init__()
+        self.user_name = user_name
+        self.password = password
+        self.ip = ip
+        self.port = port
+
+        self.connect = socket.socket()  # 创建 socket 对象
+        self.frameLen = 0
+
+        self.isConnect = True
+
+    def run(self):
+        try:
+            self.connect_button_signal.emit(False)
+            self.close_button_signal.emit(True)
+            self.connect.connect((self.ip, self.port))
+
+            message = {'code': 200, 'userName': self.user_name, 'password': self.password}  # 登录
+            self.connect.send(json.dumps(message).encode())
+
+            jsonMessage = self.connect.recv(1024).decode()
+            message = json.loads(jsonMessage)
+            print(message)
+
+            if message['code'] == 300:
+                self.log_signal.emit('登录成功')
+
+                jsonMessage = self.connect.recv(1024).decode()  # 帧数据大小
+                message = json.loads(jsonMessage)
+                print(message)
+
+                if message['code'] == 500:
+                    self.frameLen = message['data']
+                    # time.sleep(0.1)
+                    while self.isConnect:
+                        frame = self.recv_frame()
+                        # print(frame)
+                        if type(frame) == np.ndarray:
+                            self.frame_signal.emit(frame)
+
+            elif message['code'] == 301:
+                self.log_signal.emit('用户名或密码错误')
+                self.connect_button_signal.emit(True)
+                self.close_button_signal.emit(False)
+            else:
+                self.log_signal.emit(f'非预期的code {message["code"]}')
+                self.connect_button_signal.emit(True)
+                self.close_button_signal.emit(False)
+
+        except BaseException as e:
+            self.log_signal.emit(f'连接错误: {e}')
+            self.connect_button_signal.emit(True)
+            self.close_button_signal.emit(False)
+
+    def recv_frame(self):  # 根据数据长度接受一帧数据，返回 numpy.ndarray
+        receivedSize = 0
+        bytesMessage = b''
+
+        while receivedSize < self.frameLen:
+            res = self.connect.recv(8192)
+            if len(res) == 0:  # 远端shutdown或close后，不断获取到空的结果
+                self.isConnect = False
+                print(len(res))
+                break
+            receivedSize += len(res)  # 每次收到的服务端的数据有可能小于8192，所以必须用len判断
+            bytesMessage += res
+
+        # print(receivedSize)
+        if receivedSize == self.frameLen:
+            return np.frombuffer(bytesMessage, dtype=np.uint8).reshape(480, 640, 3)
+        return None
+
+    def close(self):  # 结束
+        self.isConnect = False
+        # self.connect.shutdown(2)
+        print('shutdown')
+        self.connect.close()
--- main.py
+++ main.py
@@ -8,11 +8,12 @@
 import sys
 
 import cv2
+from PyQt5 import QtGui
 from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtWidgets import QMainWindow, QApplication
 
 from clientWindow import Ui_MainWindow
-from socketFrameThread import SocketFrameThread
+from controlThread import ControlThread
 
 
 class ClientMainWindow(QMainWindow, Ui_MainWindow):
@@ -20,21 +21,21 @@ class ClientMainWindow(QMainWindow, Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
 
+        self.controlThread = None
+
         self.slot_init()
 
     def slot_init(self):
         self.connectButton.clicked.connect(self.connect_server)
 
     def connect_server(self):  # 连接服务器
-        # frameThread = Thread(target=self.socket_frame_thread)
-        # frameThread.setDaemon(True)
-        # frameThread.start()
-
-        ip = self.ipInput.text()
-        port = int(self.portInput.text())
-        self.frameThread = SocketFrameThread(ip, port)
-        self.frameThread._signal.connect(self.show_camera)  # 连接信号
-        self.frameThread.start()
+        self.controlThread = ControlThread(self.userNameInput.text(), self.passwordInput.text(),
+                                           self.ipInput.text(), int(self.portInput.text()))
+        self.controlThread.frame_signal.connect(self.show_camera)  # 连接信号
+        self.controlThread.log_signal.connect(self.print_log)
+        self.controlThread.connect_button_signal.connect(self.control_connect_button)
+        self.controlThread.close_button_signal.connect(self.control_close_button)
+        self.controlThread.start()
 
     def show_camera(self, frame):  # 显示一帧
         # print(frame)
@@ -42,6 +43,27 @@ class ClientMainWindow(QMainWindow, Ui_MainWindow):
         show = cv2.cvtColor(show, cv2.COLOR_BGR2RGB)
         showImage = QImage(show.data, show.shape[1], show.shape[0], QImage.Format_RGB888)
         self.cameraLabel.setPixmap(QPixmap.fromImage(showImage))
+
+    def print_log(self, log_str):  # UI上打印日志
+        self.log.append(log_str)
+
+    def control_connect_button(self, b):  # 控制连接按钮
+        if (self.connectButton.isEnabled() and not b) or (not self.connectButton.isEnabled() and b):
+            self.connectButton.setEnabled(b)
+
+    def control_close_button(self, b):  # 控制断开连接按钮
+        if (self.closeButton.isEnabled() and not b) or (not self.closeButton.isEnabled() and b):
+            self.closeButton.setEnabled(b)
+
+    def closeEvent(self, a0: QtGui.QCloseEvent) -> None:  # 关闭程序
+        try:
+            super().closeEvent(a0)
+            # if self.camera:
+            #     self.camera.close()
+            if self.controlThread and self.controlThread.isRunning():
+                self.controlThread.close()
+        except BaseException as e:
+            print(e)
 
 
 if __name__ == '__main__':
